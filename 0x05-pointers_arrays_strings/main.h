128798ef55bd0a80fd851fbe880f5dafcf024619
AngelOgwezzy <ogwezzyangel5@gmail.com>
2022-09-19 10:51:11 -0700

    Task 0 c array and string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,19 +1,11 @@
-#include "main.h"
-
-/**
- * _strlen - returns the length of a string
- *
- * @s: string parameter input
- *
- * Return: length of string
-*/
-
-int _strlen(char *s)
-{
-	int counter;
-
-	for (counter = 0; *s != '\0'; ++s)
-		++counter;
-
-	return (counter);
-}
+int _putchar(char c);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
