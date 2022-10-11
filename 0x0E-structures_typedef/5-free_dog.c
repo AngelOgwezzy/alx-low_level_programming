f0ee1e464adfbc1347b1544733142a9998dbb41f
AngelOgwezzy <ogwezzyangel5@gmail.com>
2022-10-11 16:49:08 -0700

    task 5 correction

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,13 +1,15 @@
-#include <stdlib.h> include "dog.h"
+#include "dog.h"
 
-/** * free_dog - free memory *@d: array * Description: Free memory for struct */
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dog
+ */
 
 void free_dog(dog_t *d)
 {
-	if (d)
-	{
-		free(d->name);
-		free(d->owner);
-		free(d);
-	}
+	if (!d)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
